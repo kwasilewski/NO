272659126356e19070ba7017af230bf5c8ca01c4
Karol Wasilewski <k.wasilewski@outlook.com>
2012-08-02 23:53:25 +0100

    Original
    Brand new

---
+++ searchform.php
@@ -0,0 +1,3 @@
+<form method="get" id="searchform" action="<?php bloginfo('url'); ?>/">
+    <input type="text" onfocus="if(this.value=='Search here') this.value='';" onblur="if(this.value=='') this.value='Search here';" value="Search here" name="s" id="s" />
+</form>
