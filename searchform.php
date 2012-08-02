ca5bffeb0a44b21f3a363162923668f8ea16f525
Karol Wasilewski <k.wasilewski@outlook.com>
2012-08-03 00:02:39 +0100

    Translation

--- searchform.php
+++ searchform.php
@@ -1,3 +1,3 @@
 <form method="get" id="searchform" action="<?php bloginfo('url'); ?>/">
-    <input type="text" onfocus="if(this.value=='Search here') this.value='';" onblur="if(this.value=='') this.value='Search here';" value="Search here" name="s" id="s" />
+    <input type="text" onfocus="if(this.value=='Szukaj...') this.value='';" onblur="if(this.value=='') this.value='Szukaj...';" value="Szukaj..." name="s" id="s" />
 </form>
